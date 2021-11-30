99539a9c63713952737f3f5b79967bc4e294cc5a
Sarthak <khandelwalsarthak0605@gmail.com>
2021-11-30 19:20:11 +0530

    Harry ka lecture no 2 tak ka hai

---
+++ sarthak.php
@@ -0,0 +1,9 @@
+<?php 
+echo "Hello world bhddsjdhbdj";
+echo 5+9888;  //103
+?>
+<!-- php is a backend language.Xamp use kerte hai ek jagah me saare cheez ko rakhne k liye.Mysql,apache server and php 
+ye sab ek baar me aa jati hai xamp se.phpmy admin is used to see the databases we have created and managing them.
+whenever making a web app using php ,make the files in htdocs which is in the c drive in xamp folder.htdocs contains all your
+code and files.koi bhi site ki files esi me banai jati hai. -->
+<!-- index automatically detect hota hai.koi aur particular file ko run kerna hai to fir '/filename' laga do website me.  -->
