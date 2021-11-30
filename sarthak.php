27cff074ad540b6eff835e2aad48161c21c4ec47
Sarthak <khandelwalsarthak0605@gmail.com>
2021-11-30 20:20:04 +0530

    lecture 4 tak done

--- sarthak.php
+++ sarthak.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Title is";echo 5+9888;  //103 ?></title>
+</head>
+<body>
 <?php 
 echo "Hello world bhddsjdhbdj";
 echo 5+9888;  //103
@@ -7,3 +16,18 @@ ye sab ek baar me aa jati hai xamp se.phpmy admin is used to see the databases w
 whenever making a web app using php ,make the files in htdocs which is in the c drive in xamp folder.htdocs contains all your
 code and files.koi bhi site ki files esi me banai jati hai. -->
 <!-- index automatically detect hota hai.koi aur particular file ko run kerna hai to fir '/filename' laga do website me.  -->
+<!-- php kabhi bbhi response me nahi aati hai koi site ko maangte waqt.html css js aati hai jo ki php bhejta hai processing ker k. -->
+<!-- apache files serve kerne me madad kerta hai -->
+<!-- php ko aap html pages k beech me embed ker k likhte ho. -->
+<!-- semicolon se php me terminate kerte hai kisi statement ko. -->
+<?php 
+echo " ghanta bhddsjdhbdj";
+echo 5+9888;  //103
+?>
+<?php 
+echo "Hello";
+echo 5+9888;  //103
+?>
+<!-- ham chahe to apna code me 10 jagah php wala tag laga ker php likh sakte hai --> 
+</body>
+</html>
